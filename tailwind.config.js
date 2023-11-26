e305146f2798685787692d7e027f114849fabe0c
Zloyyurann <77922325+Zloyyurann@users.noreply.github.com>
2023-11-26 21:09:58 +0200

    main with new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["index.html", "src/main.ts"],
+  themeConfig: {
+    logo: '/logo.svg',
+  },
   theme: {
     extend: {
       colors: {
         'background-color': '#F7EFE5',
-        'accent-color-1': '#E5D9B6',
-        'accent-color-2': '#A4BE7B',
-        'btn-color': '#5F8D4E',
-        'btn-color-off': '#285430',
+        'accent-color-1': '#DDE6ED',
+        'accent-color-2': '#9DB2BF',
+        'btn-color': '#526D82',
+        'btn-color-off': '#27374D',
       },
     },
   },
   plugins: [],
 }
-
